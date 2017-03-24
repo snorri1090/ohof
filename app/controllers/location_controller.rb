5dcfc311eac2d05695123953bcc5a0171771d058
Scott <scott@monkeyhouse-media.com>
2017-03-23 23:48:49 -0700

    updated crud with new views and controller updates

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -11,11 +11,37 @@ class LocationController < ApplicationController
   end
 
   def new
+    @location = Location.new({name: 'Default'})
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to(action: 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      redirect_to(action: 'show', id: @location.id)
+    else
+      render('edit')
+    end
   end
 
   def delete
   end
+
+  private
+    def location_params
+      params.require(:location).permit(:name, :position, :sub_location, :category, :wifi, :power, :coffee, :food)
+    end
+
 end
