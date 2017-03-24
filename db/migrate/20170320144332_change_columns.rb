d8d7cab77dd7e0bc3b096097a154cd51329dde11
Scott <scott@monkeyhouse-media.com>
2017-03-24 06:07:03 -0700

    cleaned up migrations

--- db/migrate/20170320144332_change_columns.rb
+++ db/migrate/20170320144332_change_columns.rb
@@ -1,16 +1,9 @@
 class ChangeColumns < ActiveRecord::Migration
-  def up
+  def change
     rename_column("locations", "location", "name")
     rename_column("locations", "specific_location", "sub_location")
     rename_column("locations", "location_comments", "comments")
     puts "*** Adding an index is next ***"
-    add_index("locations", "location")
-  end
-
-  def down
-    remove_index("locations", "name")
-    rename_column("locations", "comments", "location_comments")
-    rename_column("locations", "sub_location", "specific_location")
-    rename_column("locations", "name", "location")
+    add_index("locations", "name")
   end
 end
