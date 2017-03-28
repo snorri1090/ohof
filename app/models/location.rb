5ef4d34d0da2ad404a6927a48bc64bcd002187a9
Scott <scott@monkeyhouse-media.com>
2017-03-27 21:33:55 -0700

    added associates to locations and visits

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,7 @@
 class Location < ActiveRecord::Base
 
+  has_many :visits
+
   scope :sorted, lambda { order("locations.id ASC") }
   scope :newest_first, lambda { order("locations.created_at DESC") }
   scope :search, lambda {|query|
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,7 @@
 class Visit < ActiveRecord::Base
 
+  belongs_to :location
+
   scope :sorted, lambda { order("visits.id ASC") }
   scope :newest_first, lambda { order("visits.created_at DESC") }
   scope :search, lambda {|query|
