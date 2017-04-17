3058c457aa6cedc8b5ab1cd8e769efe6fdd9d701
Scott <scott@monkeyhouse-media.com>
2017-04-17 13:00:16 -0700

    added views for pages sections and subjects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,6 +22,20 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Page updated successfully."
+      redirect_to(:action => 'show', :id => @page.id)
+    else
+      render('edit')
+    end
+  end
+
   def delete
     @page = Page.find(params[:id])
   end
@@ -29,12 +43,14 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page destroyed successfully."
+    redirect_to(:action => 'index')
   end
+
 
   private
 
-  def page_params
-    params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
-  end
+    def page_params
+      params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+    end
 
 end
